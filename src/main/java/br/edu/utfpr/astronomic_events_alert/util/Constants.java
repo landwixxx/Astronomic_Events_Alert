f7417a18cd703fb109bfc1238cfef257658dd012
landwixxx <landwixxx@gmail.com>
2022-04-19 02:21:17 -0300

    Cadastro de Usuário e bug no cadastro de eventos

--- src/main/java/br/edu/utfpr/astronomic_events_alert/util/Constants.java
+++ src/main/java/br/edu/utfpr/astronomic_events_alert/util/Constants.java
@@ -1,9 +1,10 @@
 package br.edu.utfpr.astronomic_events_alert.util;
 
 public class Constants {
-    public static final String TOTAL_USERS = "totalUsers";
-    public static final String TOTAL_EVENTS = "totalEvents";
-
+    public static final String TOTAL_USERS = "usuariosTotais";
+    public static final String TOTAL_EVENTS = "alertasTotais";
     //Nome do persistence unit
     public static final String PERSISTENCE_UNIT = "aea-pu";
+    public static final String ADMIN_EMAIL = "landerwilker@yahoo.com.br";
+    public static final String ADMIN_PASSWORD = "server12345";
 }
